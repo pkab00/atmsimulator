c03044bc67a6e3174b6657c7e7f2b83d25feab1c
vbshkn <pika_pikabu@vk.com>
2025-02-13 13:49:49 +0700

    Completed SessionGUI design

--- src/main/java/com/atm/GUI/ATMSessionGUI.java
+++ src/main/java/com/atm/GUI/ATMSessionGUI.java
@@ -1,19 +1,7 @@
 package com.atm.GUI;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.GridLayout;
-
-import javax.swing.Box;
-import javax.swing.BoxLayout;
-import javax.swing.JButton;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
-import javax.swing.SwingConstants;
-
+import java.awt.*;
+import javax.swing.*;
 import com.atm.User;
 
 public class ATMSessionGUI extends CoreGUI {
@@ -23,10 +11,11 @@ public class ATMSessionGUI extends CoreGUI {
         super();
         this.user = user;
         setTitle(String.format("ATM Simulator [%s]", user.getFullName()));
+        setResizable(false);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        corePane.setLayout(new BorderLayout(5, 5));
+        corePane.setLayout(new BorderLayout());
         corePane.add(createTitleLabel(), BorderLayout.NORTH);
-        corePane.add(createBalancePanel());
+        corePane.add(createBalancePanel(), BorderLayout.CENTER);
     }
     private JLabel createTitleLabel(){
         JLabel titleLabel = new JLabel("ATM Simulator", SwingConstants.CENTER);
@@ -39,18 +28,30 @@ public class ATMSessionGUI extends CoreGUI {
     }
     private JPanel createBalancePanel(){
         JPanel balancePanel = new JPanel(new GridLayout(3, 2, 5, 5));
-        JLabel balanceLabel = new JLabel("БАЛАНС:");
+        JLabel balanceLabel = new JLabel("БАЛАНС:", SwingConstants.CENTER);
         balanceLabel.setFont(coreFont.deriveFont(16f));
-        //balancePanel.add(balanceLabel);
-
-        for(int i = 0; i < 6; i++){
-            balancePanel.add(new JButton("Кнопка №"+i));
-        }
+        balancePanel.add(balanceLabel);
 
         JTextField balanceTextField = new JTextField(String.valueOf(user.getAccount().getBalance())+" ₽");
         balanceTextField.setFont(coreFont.deriveFont(16f));
         balanceTextField.setEditable(false);
-        //balancePanel.add(balanceTextField);
+        balancePanel.add(balanceTextField);
+
+        String[] titles = {"Пополнить", "Снять", "Перевод", "Настройки"};
+        for(int i = 0; i < 4; i++){
+            JButton newButton = new JButton(titles[i]);
+            newButton.setFont(coreFont.deriveFont(16f));
+            newButton.setFocusPainted(false);
+            if(i==0 || i==3){
+                newButton.setForeground(Color.BLACK);
+                newButton.setBackground(Color.WHITE);
+            } else{
+                newButton.setForeground(Color.WHITE);
+                newButton.setBackground(Color.BLACK);
+            }
+            balancePanel.add(newButton);
+        }
+
         return balancePanel;
     }
 }
