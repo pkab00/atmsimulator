483d962a3548d6ff28b999d1a136d467bd895ee8
vbshkn <pika_pikabu@vk.com>
2025-02-14 13:04:50 +0700

    Testing & modifying Operation

--- src/main/java/com/atm/Operation.java
+++ src/main/java/com/atm/Operation.java
@@ -1,6 +1,8 @@
 package com.atm;
 
 import java.security.InvalidParameterException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Operation {
     private Account fromAccount;
@@ -25,16 +27,23 @@ public class Operation {
         validate(fromAccount, -sum);
     }
 
+    public Operation(OperationDTO DTO){
+        this.fromAccount = DTO.getFromAccount();
+        this.toAccount = DTO.getToAccount();
+        this.sum = DTO.getSum();
+        this.dateTime = DTO.getDateTime();
+    }
+
     @Override
     public String toString(){
         if(fromAccount == null){
             if(sum > 0){
-                return String.format("Зачисление %f руб. на %s", sum, toAccount.getCardNumber());
+                return String.format("Зачисление %.2f руб. на %s", sum, toAccount.getCardNumber());
             } else{
-                return String.format("Списание %f руб. с %s", sum, toAccount.getCardNumber());
+                return String.format("Списание %.2f руб. с %s", sum, toAccount.getCardNumber());
             }
         } else{
-            return String.format("Перевод %f руб. с %s на %s", sum, fromAccount.getCardNumber(), toAccount.getCardNumber());
+            return String.format("Перевод %.2f руб. с %s на %s", sum, fromAccount.getCardNumber(), toAccount.getCardNumber());
         }
     }
 
@@ -49,6 +58,17 @@ public class Operation {
                 acc.getCardNumber()));
             }
         }
+    }
+
+    public void commit(){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss dd.MM.yyyy");
+        if(fromAccount == null){
+            toAccount.changeBalance(sum);
+        } else{
+            fromAccount.changeBalance(-sum);
+            toAccount.changeBalance(sum);
+        }
+        this.dateTime = LocalDateTime.now().format(formatter);
     }
 
     public Account getFromAccount() {
@@ -69,5 +89,26 @@ public class Operation {
 
     public void setDateTime(String dateTime) {
         this.dateTime = dateTime;
+    }
+
+    public static void main(String[] args) {
+        User me = User.getExistingUser("1234", "7171 3045 4443 3989");
+        User anotherUser = User.getExistingUser("6666", "0154 0963 9160 9316");
+        Operation getSomeMoney = new Operation(me, 100);
+        getSomeMoney.commit();
+        System.out.println(getSomeMoney);
+        Operation takeSomeMoney = new Operation(anotherUser, 50);
+        takeSomeMoney.commit();
+        System.out.println(takeSomeMoney);
+        
+        System.out.println(String.format("До перевода:\n%s: %.2f\n%s: %.2f",
+        me.getFullName(), me.getAccount().getBalance(), anotherUser.getFullName(), anotherUser.getAccount().getBalance()));
+
+        Operation transaction = new Operation(me, anotherUser, 50);
+        transaction.commit();
+        System.out.println(transaction);
+
+        System.out.println(String.format("После перевода:\n%s: %.2f\n%s: %.2f",
+        me.getFullName(), me.getAccount().getBalance(), anotherUser.getFullName(), anotherUser.getAccount().getBalance()));
     }
 }
