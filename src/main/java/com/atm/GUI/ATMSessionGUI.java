fadb668dcc9f5a4e02781ab46748cbcaa9833b08
vbshkn <pika_pikabu@vk.com>
2025-02-21 13:01:50 +0700

    Implemented log out button + SessionGUI refactoring

--- src/main/java/com/atm/GUI/ATMSessionGUI.java
+++ src/main/java/com/atm/GUI/ATMSessionGUI.java
@@ -1,7 +1,7 @@
 package com.atm.GUI;
 
 import java.awt.*;
-
+import java.io.File;
 import java.util.List;
 import java.util.function.Function;
 import javax.swing.*;
@@ -15,6 +15,9 @@ import com.atm.GUI.operations.WithdrawGUI;
 public class ATMSessionGUI extends CoreGUI {
     private User user;
     private JTextField balanceTextField;
+    private GridBagConstraints gbc = new GridBagConstraints();
+    private final int COLS = 2;
+    private final int ROWS = 4;
 
     public ATMSessionGUI(User user){
         super();
@@ -36,38 +39,74 @@ public class ATMSessionGUI extends CoreGUI {
         return titleLabel;
     }
     private JPanel createBalancePanel(){
-        JPanel balancePanel = new JPanel(new GridLayout(3, 2, 5, 5));
+        JPanel mainPanel = new JPanel(new GridBagLayout());
         JLabel balanceLabel = new JLabel("БАЛАНС:", SwingConstants.CENTER);
-        balanceLabel.setFont(coreFont.deriveFont(16f));
-        balancePanel.add(balanceLabel);
+        balanceLabel.setFont(coreFont.deriveFont(20f));
+
+        gbc.fill = GridBagConstraints.BOTH;
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.weightx = 1.0;
+        gbc.weighty = 1.0;
+        mainPanel.add(balanceLabel, gbc);
 
         balanceTextField = new JTextField(String.valueOf(user.getAccount().getBalance())+" ₽");
-        balanceTextField.setFont(coreFont.deriveFont(16f));
+        balanceTextField.setFont(coreFont.deriveFont(20f));
         balanceTextField.setEditable(false);
-        balancePanel.add(balanceTextField);
+
+        gbc.fill = GridBagConstraints.BOTH;
+        gbc.gridx = 1;
+        gbc.gridy = 0;
+        gbc.weightx = 1.0;
+        gbc.weighty = 1.0;
+        mainPanel.add(balanceTextField, gbc);
 
         List<String> titles = List.of("Пополнить", "Снять", "Перевод", "История опреаций");
         List<Function<ATMSessionGUI, UserInputGUI>> constructors = List.of(
             DepositGUI::new, WithdrawGUI::new,
             TransactionGUI::new, OperationLogGUI::new
         );
-        for(int i = 0; i < 4; i++){
-            JButton newButton = new JButton(titles.get(i));
-            newButton.setFont(coreFont.deriveFont(16f));
-            newButton.setFocusPainted(false);
-            if(i==0 || i==3){
-                newButton.setForeground(Color.BLACK);
-                newButton.setBackground(Color.WHITE);
-            } else{
-                newButton.setForeground(Color.WHITE);
-                newButton.setBackground(Color.BLACK);
+        int i = 0;
+        for(int y = 1; y < ROWS-1; y++){
+            for(int x = 0; x < COLS; x++){
+                final int index = i;
+                JButton newButton = new JButton();
+                newButton.setBackground((y%2==0) ? Color.BLACK : Color.WHITE);
+                newButton.setForeground((y%2==0) ? Color.WHITE : Color.BLACK);
+                newButton.addActionListener((e) -> constructors.get(index).apply(this));
+                newButton.setBorderPainted(false);
+                newButton.setText(titles.get(i++));
+                newButton.setFont(coreFont.deriveFont(26f));
+
+                gbc.fill = GridBagConstraints.BOTH;
+                gbc.gridx = x;
+                gbc.gridy = y;
+                gbc.weightx = 1.0;
+                gbc.weighty = 1.0;
+                mainPanel.add(newButton, gbc);
             }
-            final var constructor = constructors.get(i);
-            newButton.addActionListener((e) -> constructor.apply(this));
-            balancePanel.add(newButton);
         }
 
-        return balancePanel;
+        JButton logoutButton = new JButton(new ImageIcon(GRAPHICS+"//logout.png"));
+        logoutButton.setText("Выйти из аккаунта");
+        logoutButton.setFont(coreFont.deriveFont(20f));
+        logoutButton.setForeground(Color.BLACK);
+        logoutButton.setBackground(Color.WHITE);
+        logoutButton.setBorderPainted(false);
+        logoutButton.addActionListener((e) ->{
+            File savedData = new File("src\\main\\resources\\ser\\_user.ser");
+            if(savedData.exists()) savedData.delete();
+            dispose();
+            new LoginGUI();
+        });
+
+        gbc.fill = GridBagConstraints.BOTH;
+        gbc.gridx = 0;
+        gbc.gridy = ROWS;
+        gbc.gridwidth = 2;
+        mainPanel.add(logoutButton, gbc);
+
+        return mainPanel;
     }
 
     public void updateBalanceLabel(){
